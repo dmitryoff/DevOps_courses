68e10a45201d350729857e8682f03d803af55e96
dmitry <supranovich2@gmail.com>
2021-11-06 13:09:03 +0300

    terraform aws security group

---
+++ 6.Cloudformation-Terraform/2.security-group.tf
@@ -0,0 +1,52 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "web-sg-terraform"
+  description = "SSH-HTTP-HTTPS"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "sg_terraform"
+  }
+}
+
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+resource "aws_security_group_rule" "public_out" {
+  type              = "egress"
+  to_port           = 0
+  protocol          = "-1"
+  from_port         = 0
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_tls.id
+}
+
+  resource "aws_security_group_rule" "private_in_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+ 
+  security_group_id = aws_security_group.allow_tls.id
+}
+  
+  resource "aws_security_group_rule" "private_in_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+ 
+  security_group_id = aws_security_group.allow_tls.id
+}
+
+  resource "aws_security_group_rule" "private_in_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+ 
+  security_group_id = aws_security_group.allow_tls.id
+}
