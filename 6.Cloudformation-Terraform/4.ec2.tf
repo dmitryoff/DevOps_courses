607b63f79cf434d99d1ae6a7f5523afd80acf915
dmitry <supranovich2@gmail.com>
2021-11-08 21:50:09 +0300

    terraform correct

--- 6.Cloudformation-Terraform/4.ec2.tf
+++ 6.Cloudformation-Terraform/4.ec2.tf
@@ -27,5 +27,5 @@ resource "aws_instance" "instance_2" {
 }
 
 resource "aws_key_pair" "keyterraform" {
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCn1Ka35J+AadZ3K0m8e0cl8LQFbWtMjfI90wugKcLZEZVBfUB/W6K5lAf5MYx5TOvvzt2KAtUS/d8lULs0pl9ZvZ728DVAC6Rhr98pj+bEeTp4mgPRBI/CP0Ghc0j49TXy/reB76/hiiNiJqnFIjXS+0xBsFNO0XkU+VJoXLB9DaaTHPJqap0+hM0xTYsUC8me5sDJJvsaixpkRQfuA/5p8eX01zGFyFEzNmqzAiD1ybtDGoVIp1YDqJy0TgR8tpzs1dD4xIBZslJ7gpS89OG/47NsjRKbHl2djF3qjFkjYtGSMeHnfkCm+7VqFRYmJWnX4tdTel23GAGxOKMEZb+Qb4OVQz7wywzxGKcVNTwQZq9D/f01/+t0/7T2ab7kSCKmrmG9474clOBKN06iO/I1Ju5+bhRG5mhX/ocnsKNSgamKjXSSdO4vXAFkzHUKci+XqmzgkhySdnXpvIEMU/NpLUBxvweBNAoef8D0RBAcsF5oIg/nS7/ZdVcM+SZQl4E= supranovcih2@gmail.com"
+  public_key = "ssh-rsa ***"
 }
